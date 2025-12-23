7e899c6559b8d028f059e4eb5cf0c790c1bcf104
eloise-bst <eloise.brest@epfedu.fr>
2025-12-23 20:59:43 +0100

    implémentation de chacune des méthodes

--- LightsOff_BREST_version_console/src/lightsoff_brest_version_console/GrilleDeCellules.java
+++ LightsOff_BREST_version_console/src/lightsoff_brest_version_console/GrilleDeCellules.java
@@ -4,6 +4,8 @@
  */
 package lightsoff_brest_version_console;
 
+import java.util.Random;
+
 /**
  *
  * @author eloisebrest
@@ -13,7 +15,7 @@ public class GrilleDeCellules {
     private CelluleLumineuse[][] matriceCellules;
     private int nbLignes;
     private int nbColonnes;
-    
+
     public GrilleDeCellules(int p_nbLignes, int p_nbColonnes) {
         nbLignes = p_nbLignes;
         nbColonnes = p_nbColonnes;
@@ -26,29 +28,74 @@ public class GrilleDeCellules {
             }
         }
     }
-    
+
     @Override
     public String toString() {
-    String chaine = "    ";
-    for (int j = 0; j < nbColonnes; j++) {
-        chaine += j + " ";
-    }
-    chaine += "\n";
-
-    chaine += "  |";
-    for (int j = 0; j < nbColonnes; j++) {
-        chaine += "--";
-    }
-    chaine += "\n";
-
-    for (int i = 0; i < nbLignes; i++) {
-        chaine += i + " | ";
+        String chaine = "    ";
         for (int j = 0; j < nbColonnes; j++) {
-            chaine += matriceCellules[i][j].toString() + " "; 
+            chaine += j + " ";
         }
         chaine += "\n";
+
+        chaine += "  |";
+        for (int j = 0; j < nbColonnes; j++) {
+            chaine += "--";
+        }
+        chaine += "\n";
+
+        for (int i = 0; i < nbLignes; i++) {
+            chaine += i + " | ";
+            for (int j = 0; j < nbColonnes; j++) {
+                chaine += matriceCellules[i][j] + " ";
+            }
+            chaine += "\n";
+        }
+        return chaine;
     }
-    
-    return chaine;
-}
+
+    public void activerLigneDeCellules(int idLigne) {
+        for (int j = 0; j < nbColonnes; j++) {
+            matriceCellules[idLigne][j].activerCellule();
+        }
+    }
+
+    public void activerColonneDeCellules(int idColonne) {
+        for (int i = 0; i < nbLignes; i++) {
+            matriceCellules[i][idColonne].activerCellule();
+        }
+    }
+
+    public void activerDiagonaleDescendante() {
+        int taille = Math.min(nbLignes, nbColonnes);
+        for (int i = 0; i < taille; i++) {
+            matriceCellules[i][i].activerCellule();
+        }
+    }
+
+    public void activerDiagonaleMontante() {
+        int taille = Math.min(nbLignes, nbColonnes);
+        for (int i = 0; i < taille; i++) {
+            matriceCellules[i][nbColonnes - 1 - i].activerCellule();
+        }
+    }
+
+    public void activerLigneColonneOuDiagonaleAleatoire() {
+        Random rand = new Random();
+        int choix = rand.nextInt(4);
+
+        switch (choix) {
+            case 0:
+                activerLigneDeCellules(rand.nextInt(nbLignes));
+                break;
+            case 1:
+                activerColonneDeCellules(rand.nextInt(nbColonnes));
+                break;
+            case 2:
+                activerDiagonaleDescendante();
+                break;
+            case 3:
+                activerDiagonaleMontante();
+                break;
+        }
+    }
 }
--- LightsOff_BREST_version_console/src/lightsoff_brest_version_console/LightsOff_BREST_version_console.java
+++ LightsOff_BREST_version_console/src/lightsoff_brest_version_console/LightsOff_BREST_version_console.java
@@ -17,6 +17,26 @@ public class LightsOff_BREST_version_console {
         
         GrilleDeCellules grille = new GrilleDeCellules(5, 5);
         System.out.println(grille);
+
+        GrilleDeCellules g1 = new GrilleDeCellules(5, 5);
+        g1.activerLigneDeCellules(2);
+        System.out.println(g1);
+
+        GrilleDeCellules g2 = new GrilleDeCellules(5, 5);
+        g2.activerColonneDeCellules(3);
+        System.out.println(g2);
+
+        GrilleDeCellules g3 = new GrilleDeCellules(5, 5);
+        g3.activerDiagonaleDescendante();
+        System.out.println(g3);
+
+        GrilleDeCellules g4 = new GrilleDeCellules(5, 5);
+        g4.activerDiagonaleMontante();
+        System.out.println(g4);
+
+        GrilleDeCellules g5 = new GrilleDeCellules(5, 5);
+        g5.activerLigneColonneOuDiagonaleAleatoire();
+        System.out.println(g5);
     }
     
 }
