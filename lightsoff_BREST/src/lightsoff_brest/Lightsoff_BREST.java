4db34a71187546687e7406865b6470937bd24614
eloise-bst <eloise.brest@epfedu.fr>
2025-12-23 09:57:44 +0100

    création du projet

---
+++ lightsoff_BREST/src/lightsoff_brest/Lightsoff_BREST.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightsoff_brest;
+
+/**
+ *
+ * @author eloisebrest
+ */
+public class Lightsoff_BREST {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
