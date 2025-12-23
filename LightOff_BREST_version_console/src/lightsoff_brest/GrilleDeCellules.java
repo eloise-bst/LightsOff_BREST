02f3ced741c5f0d5e9a2c346ce66d6baff9a5846
eloise-bst <eloise.brest@epfedu.fr>
2025-12-23 10:34:29 +0100

    création grilleDeCellules et ajout de tous les attributs

---
+++ LightOff_BREST_version_console/src/lightsoff_brest/GrilleDeCellules.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightsoff_brest;
+
+/**
+ *
+ * @author eloisebrest
+ */
+public class GrilleDeCellules {
+    
+    private CelluleLumineuse[][] matriceCellules;
+    private int nbLignes;
+    private int nbColonnes;
+}
